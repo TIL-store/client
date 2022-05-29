5da46c23106e8060bb766714ab5bb6679f032be3
goum <40739041+goumi1009@users.noreply.github.com>
2022-05-29 21:16:14 +0900

    Ts 32 msw setting (#4)
    * chore: 필요 없는 내용 삭제
    
    * feat: 컨벤션 기본 세팅
    
    * feat: commit lint 추가
    
    * fix: lint error 수정
    
    - theme 변수를 export default로 바꿔줌
    
    * feat: TS-32 MSW init
    
    Co-authored-by: limkhl <limkh.dev@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import './index.css';
 import theme from './theme';
+import worker from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start().catch(() => {
+    console.log('😥');
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
