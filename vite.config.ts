c582a507c7ccb0dd865b0b00e41dd50376418a3a
goum <40739041+goumi1009@users.noreply.github.com>
2022-06-06 16:07:54 +0900

    TS-39 절대 경로 설정 값 변경 (#6)
    fix: import 절대경로 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@mocks',
+        replacement: resolve(__dirname, 'src/mocks'),
+      },
+      {
+        find: '@pages',
+        replacement: resolve(__dirname, 'src/pages'),
+      },
+    ],
+  },
 });
